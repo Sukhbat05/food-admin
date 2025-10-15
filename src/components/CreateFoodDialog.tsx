81086bc00fae6d5d2751ef7ec4e248f546676844
Sukhbat05 <asukhbat27@gmail.com>
2025-10-15 14:52:20 +0800

    j2.1

--- src/components/CreateFoodDialog.tsx
+++ src/components/CreateFoodDialog.tsx
@@ -1,141 +1,93 @@
+"use client";
 
+import { useState } from "react";
 
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { ChangeEvent, useState } from "react";
+export default function AddFoodForm() {
+  const [foodName, setFoodName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [ingredients, setIngredients] = useState("");
+  const [category, setCategory] = useState("68eeffcbd5fdbd319a11f9d4");
+  const [image, setImage] = useState("");
+  const [responseMessage, setResponseMessage] = useState("");
 
-export const CreateFoodDialog = () => {
-  const [image, setImage] = useState<File | undefined>();
-  const [name, setName] = useState<string>("");
-  const [price, setPrice] = useState<number>(0);
-  const [ingredients, setIngredients] = useState<string>("");
-  const [category, setCategory] = useState<string>("");
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-  const addFoodHandler = async () => {
-    if (!name || !price || !image || !ingredients || !category) {
-      alert("All fields are required");
-      return;
-    }
-
-    const form = new FormData();
-
-    form.append("name", name);
-    form.append("price", String(price));
-    form.append("image", image); // File object
-    form.append("ingredients", ingredients);
-    form.append("category", category);
+    const foodData = {
+      foodName,
+      price,
+      ingredients,
+      category,
+      image,
+    };
 
     try {
-      const response = await fetch("http://localhost:4000/api/food", {
+      const res = await fetch("http://localhost:4000/api/food", {
         method: "POST",
-        body: form,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(foodData),
       });
 
-      const data = await response.json();
-      if (response.ok) {
-        alert("Food created successfully!");
-        setName("");
-        setPrice(0);
-        setImage(undefined);
-        setIngredients("");
-        setCategory("");
+      const result = await res.json();
+
+      if (res.ok) {
+        setResponseMessage("Success");
       } else {
-        alert(data.error || "Failed to create food");
+        setResponseMessage(`Error: ${result.message}`);
       }
-    } catch (error) {
-      alert("Failed to create food");
+    } catch (err) {
+      setResponseMessage("Holbogdhgui");
     }
   };
-  const nameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-  const priceChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setPrice(Number(e.target.value));
-  };
-  const fileChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setImage(e.target.files[0]);
-    }
-  };
-  const ingredientsChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setIngredients(e.target.value);
-  };
-  const categoryChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setCategory(e.target.value);
-  };
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Open Dialog</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Create Food</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4">
-          <div className="grid gap-3">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              name="name"
-              defaultValue={name}
-              value={name}
-              onChange={nameChangeHandler}
-            />
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="price">Price</Label>
-            <Input
-              id="price"
-              name="price"
-              type="number"
-              defaultValue="0"
-              value={price}
-              onChange={priceChangeHandler}
-            />
-          </div>
-          <div className="grid w-full max-w-sm items-center gap-3">
-            <Label htmlFor="picture">Picture</Label>
-            <Input id="picture" type="file" onChange={fileChangeHandler} />
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="ingredients">Ingredients</Label>
-            <Input
-              id="ingredients"
-              name="ingredients"
-              value={ingredients}
-              onChange={ingredientsChangeHandler}
-            />
-          </div>
-          <div className="grid gap-3">
-            <Label htmlFor="category">Category</Label>
-            <Input
-              id="category"
-              name="category"
-              value={category}
-              onChange={categoryChangeHandler}
-            />
-          </div>
-          <Button
-            type="submit"
-            size={"sm"}
-            className="w-fit px-4 py-[10px]"
-            onClick={addFoodHandler}
-          >
-            <p className="leading-5"> Save changes</p>
-          </Button>
-        </div>
-        <DialogFooter></DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <form onSubmit={handleSubmit} className="p-4 w-80 mt-10 my-10">
+      <input
+        type="text"
+        placeholder="Name"
+        value={foodName}
+        onChange={(e) => setFoodName(e.target.value)}
+        className="w-full border p-2"
+        required
+      />
+      <input
+        type="number"
+        placeholder="price"
+        value={price}
+        onChange={(e) => setPrice(Number(e.target.value))}
+        className="w-full border p-2"
+        required
+      />
+      <input
+        type="text"
+        placeholder="ingridients"
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+        className="w-full border p-2"
+        required
+      />
+      <input
+        type="text"
+        placeholder="Category ID"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        className="w-full border p-2"
+        required
+      />
+      <input
+        type="file"
+        placeholder="Image"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+        className="w-full border p-2"
+        required
+      />
+      <button type="submit" className="bg-blue-600 text-white px-4 py-2">
+        Save
+      </button>
+      {responseMessage && <p className="text-sm mt-2">{responseMessage}</p>}
+    </form>
   );
-};
+}
